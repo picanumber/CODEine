5e38901da55bbe51cabbf3785c1f56ba0116ab29
picanumber <picanumber@gmail.com>
2015-03-01 20:15:41 +0200

    Add integer range - code cleanup and organization

--- integer_sequence.h
+++ integer_sequence.h
@@ -1,66 +1,61 @@
 #ifndef I_SEQ_2908SDGF375
 #define I_SEQ_2908SDGF375
 
+#include <type_traits>
 
-namespace seq
-{
+namespace cti 
+{ // compile time integers
+
 	// a sequence container ---------------------------------------------
 	template<int ...>
-	struct ints
-	{
-	};
+	struct ints { };
 
-	// integer sequence -------------------------------------------------
-	template<int N, int... Is>
-	struct int_seq : int_seq < N - 1, N, Is... >
+	namespace detail
 	{
-	};
+		// integer sequence ---------------------------------------------
+		template<int N, int... Is>
+		struct int_seq : int_seq < N - 1, N, Is... > { };
 
-	template<int... Is>
-	struct int_seq < 0, Is... >
-	{
-		using type = ints < 0, Is... > ;
-	};
-	
-	// convenience alias ----------------------------
+		template<int... Is>
+		struct int_seq<0, Is...>
+		{
+			using type = ints < 0, Is... > ;
+		};
+
+		// reverse integer sequence -------------------------------------
+		template<int C, int N, int... Is>
+		struct rev_seq : rev_seq<C - 1, N, N - C, Is...> { };
+
+		template<int N, int... Is>
+		struct rev_seq<0, N, Is...>
+		{
+			using type = ints<N, Is...> ;
+		};
+
+		// integer range ------------------------------------------------
+		template<int C, int N0, int N, int... Is>
+		struct int_range : 
+			int_range<C, N0, (N - C > N0 ? N - C : N0), N, Is...> { };
+
+		template<int C, int N0, int... Is>
+		struct int_range<C, N0, N0, Is...>
+		{
+			using type = ints<N0, Is...>;
+		};
+	}
+
+	// convenience aliases ----------------------------------------------
 	template<int N>
-	using IS = typename int_seq<N>::type;
-	// ------------------------------------------------------------------
-
-	// reverse integer sequence -----------------------------------------
-	template<int C, int N, int... Is>
-	struct rev_seq : rev_seq < C - 1, N, N - C, Is... >
-	{
-	};
-
-	template<int N, int... Is>
-	struct rev_seq < 0, N, Is... >
-	{
-		using type = ints < N, Is... > ;
-	};
+	using sequence = typename detail::int_seq<N>::type;
 	
-	// convenience alias ----------------------------
 	template<int N>
-	using RS = typename rev_seq<N, N>::type;
-	// ------------------------------------------------------------------
+	using reverse_sequence = typename detail::rev_seq<N, N>::type;
 
-	// integer range ----------------------------------------------------
-	template<int N0, int N, int... Is>
-	struct int_range : int_range < N0, N - 1, N, Is... >
-	{
-	};
+	template<int N0, int N, 
+		typename std::enable_if<(N0 <= N), int>::type C = 1>
+	using range = typename detail::int_range<C, N0, N - ((N-N0)%C)>::type;
 
-	template<int N0, int... Is>
-	struct int_range < N0, N0, Is... >
-	{
-		using type = ints < N0, Is... >;
-	};
-
-	// convenience alias ----------------------------
-	template<int N0, int N>
-	using IR = typename int_seq<N0, N>::type;
-	// ------------------------------------------------------------------
-}
-
+} // ~compile time integers
 
 #endif
+
