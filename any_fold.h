fe7b8f3eb3c9adea6863d6903b47f81b2ce4672d
picanumber <picanumber@gmail.com>
2015-12-15 02:58:00 +0200

    add skeleton sources

---
+++ any_fold.h
@@ -0,0 +1,14 @@
+#ifndef ANY_FOLD_HPP_25L3KJ
+#define ANY_FOLD_HPP_25L3KJ
+
+
+
+
+#endif
+
+
+
+
+
+
+
---
+++ cpp_17.h
@@ -0,0 +1,18 @@
+#ifndef CPP_17_HL3_KJPP_25
+#define CPP_17_HL3_KJPP_25
+
+
+
+
+#endif
+
+
+
+
+
+
+
+
+
+
+
